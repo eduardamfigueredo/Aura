e2c89078dfe6cbbafdce1e04950a0e6715ed0738
felipe <felipesauma23@gmail.com>
2026-06-01 00:28:41 -0300

    corrigindo erros

--- app/src/main/java/com/example/aura/HomeFragment.kt
+++ app/src/main/java/com/example/aura/HomeFragment.kt
@@ -1,15 +1,14 @@
 package com.example.aura
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import com.example.aura.ui.screens.ForumFragment
+import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class HomeFragment : Fragment() {
+class  HomeFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
